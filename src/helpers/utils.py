4b30d8b16a6defd847133dddf686d0418599b996
Karan Mehra <33761778+ernestomehra@users.noreply.github.com>
2021-08-11 20:14:01 +0530

    add function help

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -2,9 +2,17 @@ import requests
 
 
 def get_data(url, params):
+    """
+    url: Add the url endpoint to get API response from
+    params: params to be sent alongwith the GET request.
+    """
     data = requests.get(url=url, params=params)
     return data
 
 
 def get_json_data(response, key):
+    """
+    get response in JSON data format, pass response as an argument.
+    key: get the value for key passed into as an argument
+    """
     return response.json()[key]
